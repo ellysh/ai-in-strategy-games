fea0a15d6a97853b7148435670645b27c5d671e1
Ilya Shpigor <petrsum@gmail.com>
2022-01-22 15:47:45 +0300

    Continue the "Appendix" section

--- manuscript/resources/code/ArtificialIntelligence/k-mean-clustering.py
+++ manuscript/resources/code/ArtificialIntelligence/k-mean-clustering.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 
-from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs
-
+from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
 X, y_true = make_blobs(n_samples=300, centers=50,
                        cluster_std=2.00, random_state=0,
                        center_box=(4, 50))
 
-kmeans = KMeans(n_clusters=4, random_state=0).fit(X)
-kmeans.labels_
+kmeans = KMeans(n_clusters=4, random_state=0)
+kmeans.fit(X)
 
 plt.figure(figsize=(8, 6))
 plt.scatter(X[:,0], X[:,1], c=kmeans.labels_.astype(float))
@@ -21,4 +20,3 @@ plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5);
 plt.xlabel("Просмотры категории автозапчасти")
 plt.ylabel("Просмотры категории велозапчасти")
 plt.show()
-
