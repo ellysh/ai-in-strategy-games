d395b0bfa7ffa9f8faf28a1510da776afa368684
Ilya Shpigor <petrsum@gmail.com>
2023-11-12 12:56:57 +0100

    Fix the version generator script

--- manuscript/update-version.sh
+++ manuscript/update-version.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "## Версия" > version.md
-echo -n "# " >> version.md
+echo "### Версия" > version.md
 git show --no-patch --format=%ci HEAD >> version.md
-echo -n "# " >> version.md
 git rev-parse HEAD >> version.md
