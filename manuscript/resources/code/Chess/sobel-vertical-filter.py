ed33e5b55c79cdc0253ed3dd24acef79b94b2ec7
Ilya Shpigor <petrsum@gmail.com>
2023-12-10 18:10:52 +0100

    Continue the "Appendix" section

--- manuscript/resources/code/Chess/sobel-vertical-filter.py
+++ manuscript/resources/code/Chess/sobel-vertical-filter.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
 
 import numpy as np
-from matplotlib.pyplot import imread
 import matplotlib.pyplot as plt
 from scipy.ndimage.filters import convolve
 
+# Объявить двумерный массив 3x3 с вертикальным фильтром Собеля
 sobel_vertical = np.array([ [1, 2, 1], [0, 0, 0], [-1, -2, -1] ])
 
-img = imread("lena.png")
+# Загрузить фотографию Лены
+img = plt.imread("lena.png")
 
+# Применить фильтр к каждому из 3-х каналов цвета фотографии
 channels = []
 for channel in range(3):
-    res = convolve(img[:,:,channel], sobel_vertical)
-    channels.append(res)
+    result = convolve(img[:,:,channel], sobel_vertical)
+    channels.append(result)
 
+# Скомбинировать 3 карты признаков в RGB изображение для визуализации
 img = np.dstack((channels[0], channels[1], channels[2]))
-plt.imshow(img, cmap='gray')
+
+# Визуализировать изображение с картами признаков
+plt.imshow(img)
+
+# Открыть окно с изображением
 plt.show()
