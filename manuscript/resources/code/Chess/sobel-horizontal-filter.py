ef3bbc5f1b50fec12a29994bc90a01bcf9d157a0
Ilya Shpigor <petrsum@gmail.com>
2023-12-10 18:06:15 +0100

    Fix the scripts for the "Chess" chapter

--- manuscript/resources/code/Chess/sobel-horizontal-filter.py
+++ manuscript/resources/code/Chess/sobel-horizontal-filter.py
@@ -5,13 +5,13 @@ from matplotlib.pyplot import imread
 import matplotlib.pyplot as plt
 from scipy.ndimage.filters import convolve
 
-sobel_vertical = np.array([ [1, 2, 1], [0, 0, 0], [-1, -2, -1] ])
+sobel_horizontal = np.array([ [1, 0, -1], [2, 0, -2], [1, 0, -1] ])
 
 img = imread("lena.png")
 
 channels = []
 for channel in range(3):
-    res = convolve(img[:,:,channel], sobel_vertical)
+    res = convolve(img[:,:,channel], sobel_horizontal)
     channels.append(res)
 
 img = np.dstack((channels[0], channels[1], channels[2]))
